a5776ac9951d712b89a6d7d35f2902d5da475968
Jonathan Malave <jmaxmalave@aol.com>
2025-10-14 15:08:32 -0400

    fixes

--- src/App.js
+++ src/App.js
@@ -1,46 +1,50 @@
 import React, { Component } from "react";
-import * as d3 from "d3";
+import "./App.css";
+import * as d3 from "d3"
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      data: [
-        { technology: "Excel", count: 1078 },
-        { technology: "Tableau", count: 852 },
-        { technology: "PowerPoint", count: 681 },
-        { technology: "R", count: 561 },
-        { technology: "Python", count: 530 }
-      ]
-    };
+    this.state = {wordFrequency:[]};
+  }
+  componentDidMount() {
+    this.renderChart();
   }
 
-  componentDidMount() {
-    // Create a div element and append it to the body. This div will be positioned absolutely and initially hidden
-    const tooltip = d3.select("body").selectAll(".tooltip").data([0]).join("div").attr("class", "tooltip").style("position", "absolute") // for precise positioning
-      .style("background", "black").style("color", "white").style("padding", "18px").style("border-radius", "4px").style("font-size", "12px").style("display", "none");
+  componentDidUpdate() {
+    this.renderChart();
+  }
+
+  getWordFrequency = (text) => {
+    const stopWords = new Set(["the", "and", "a", "an", "in", "on", "at", "for", "with", "about", "as", "by", "to", "of", "from", "that", "which", "who", "whom", "this", "these", "those", "it", "its", "they", "their", "them", "we", "our", "ours", "you", "your", "yours", "he", "him", "his", "she", "her", "hers", "it", "its", "we", "us", "our", "ours", "they", "them", "theirs", "I", "me", "my", "myself", "you", "your", "yourself", "yourselves", "was", "were", "is", "am", "are", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "as", "if", "each", "how", "which", "who", "whom", "what", "this", "these", "those", "that", "with", "without", "through", "over", "under", "above", "below", "between", "among", "during", "before", "after", "until", "while", "of", "for", "on", "off", "out", "in", "into", "by", "about", "against", "with", "amongst", "throughout", "despite", "towards", "upon", "isn't", "aren't", "wasn't", "weren't", "haven't", "hasn't", "hadn't", "doesn't", "didn't", "don't", "doesn't", "didn't", "won't", "wouldn't", "can't", "couldn't", "shouldn't", "mustn't", "needn't", "daren't", "hasn't", "haven't", "hadn't"]);
+    const words = text.toLowerCase().replace(/[.,/#!$%^&*;:{}=_`~()]/g, "").replace(/\s{2,}/g, " ").split(" ");
+    const filteredWords = words.filter(word => !stopWords.has(word));
+    return Object.entries(filteredWords.reduce((freq, word) => {
+      freq[word] = (freq[word] || 0) + 1;
+      return freq;
+    }, {}));
+  }
 
 
-      d3.selectAll("rect").data(this.state.data).join("rect").attr("x",0).attr("y",(d,i)=>i*51).attr("width",d=>d.count).attr("height",50).attr("fill","orange")
-      .on("mouseover",(event, d)=>{
-        tooltip.style("display","block").style("left", event.pageX + "px").style("top",event.pageY + "px").html(`<p> Technology: ${d.technology} </p> <p> Count: ${d.count} </p>`)
-
-      })
-      .on("mouseout", ()=>{
-        tooltip.style("display","none")
-      })
-    }
+  renderChart() {
+    const data = this.state.wordFrequency.sort((a,b)=>b[1]-a[1]).slice(0,5)
+    console.log(data)
+    // your code here
+    
+  }
 
   render() {
     return (
-      <div className="responsive-svg-container">
-        <svg viewBox="0 0 1200 1600">
-          <rect></rect>
-          <rect></rect>
-          <rect></rect>
-          <rect></rect>
-          <rect></rect>
-        </svg>
+      <div className="parent">
+        <div className="child1" style={{width: 1000 }}>
+        <textarea type="text" id="input_field" style={{ height: 150, width: 1000 }}/>
+          <button type="submit" value="Generate Matrix" style={{ marginTop: 10, height: 40, width: 1000 }} onClick={() => {
+                var input_data = document.getElementById("input_field").value
+                this.setState({wordFrequency:this.getWordFrequency(input_data)})
+              }}
+            > Generate WordCloud</button>
+        </div>
+        <div className="child2"><svg className="svg_parent"></svg></div>
       </div>
     );
   }
